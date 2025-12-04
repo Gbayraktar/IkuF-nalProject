009e1d9267749883e2eaa12ba7c924f35e380cbb
Muhammet Yıldız <eahm25yildiz@gmail.com>
2025-12-05 01:35:30 +0300

    yeni push salıyom ben kafi

--- İkuFinalProject/Assets/mami/Scripts/PlayerMovement_01.cs
+++ İkuFinalProject/Assets/mami/Scripts/PlayerMovement_01.cs
@@ -4,10 +4,13 @@ public class PlayerMovement_01 : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -20,15 +23,27 @@ public class PlayerMovement_01 : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
 
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
-        }
     }
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Ground") 
+            grounded = true;
+    }
+
 }
 
